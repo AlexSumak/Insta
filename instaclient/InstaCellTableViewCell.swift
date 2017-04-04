ed68eb77a0bf58cc8fdedf809122d51cdc46cfb1
Alex Sumak <alex.sumak@Alex-Sumaks-MacBook-Air.local>
2017-04-04 15:05:59 -0700

    first commit

---
+++ instaclient/InstaCellTableViewCell.swift
@@ -0,0 +1,24 @@
+//
+//  InstaCellTableViewCell.swift
+//  instaclient
+//
+//  Created by  Alex Sumak on 4/1/17.
+//  Copyright © 2017  Alex Sumak. All rights reserved.
+//
+
+import UIKit
+
+class InstaCellTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
